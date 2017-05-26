c6ee851cf9679e985dfab747fe1264677df42a8b
Harold Simpson <harold.simpson@storyful.com>
2017-05-26 12:42:31 +0100

    People full name spec

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe Person, type: :model do
     person.valid?
     expect(person.errors[:first_name]).to include("can't be blank")
   end
+
+  it 'can construct a full name from the first and last names' do
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    person = Person.new(first_name: first_name, last_name: last_name)
+    expect(person.full_name).to eq("#{first_name} #{last_name}")
+  end
 end
